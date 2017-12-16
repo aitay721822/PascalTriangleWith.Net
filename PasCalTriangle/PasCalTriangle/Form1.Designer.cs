a19b2631eb481d23411084dda97005928ccacb7d
aitay721822 <aitay721822@gmail.com>
2017-12-16 16:44:02 +0800

    Add files via upload

---
+++ PasCalTriangle/PasCalTriangle/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace PasCalTriangle
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 10);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(179, 22);
+            this.textBox2.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(197, 10);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Input";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Font = new System.Drawing.Font("標楷體", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.textBox1.Location = new System.Drawing.Point(12, 38);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(260, 213);
+            this.textBox1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "PasCalTriangle";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ PasCalTriangle/PasCalTriangle/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PasCalTriangle
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public class PascalTriangle
+        {
+            /// <summary>
+            /// 輸出n層巴斯卡三角形
+            /// </summary>
+            /// <param name="n">層級</param>
+            /// <param name="output">輸出</param>
+            public void CreatePascal(int n, TextBox output)
+            {
+                if (n >= 0)
+                {
+                    output.TextAlign = HorizontalAlignment.Center; //把textbox設置為置中（比較整齊）
+                    int[,] Pascal = CreatePascalTri(n);            //製造巴斯卡三角形矩陣
+                    int Z = calcBits(Pascal);                      //計算最大位元
+                    string Str = string.Empty;                     //建立一變數
+                    Str = Str.PadLeft(Z, '0');                     //用來補0
+                    int i = 0, j = i;
+                    int now_Step = 0;
+                    while (now_Step != n)
+                    {
+                        for (i = now_Step, j = 0; i >= 0 && j <= now_Step; i--, j++)
+                        {
+                            output.Text += Pascal[i, j].ToString(Str) + " ";
+                        }
+                        output.Text += "\r\n";
+                        now_Step++;
+                    }
+                }
+            }
+
+            /// <summary>
+            /// 計算在n層巴斯卡三角形裡面出現最大的數值為幾位數
+            /// </summary>
+            /// <param name="Pascal">巴斯卡三角形矩陣</param>
+            /// <returns>最大位數</returns>
+            private int calcBits(int[,] Pascal)
+            {
+                int max = 1;
+                for (int i = 0; i < Pascal.GetLength(0); i++)
+                {
+                    for (int j = 0; j < Pascal.GetLength(1) - i; j++)
+                    {
+                        if (max < Pascal[i, j].ToString().Length)
+                            max = Pascal[i, j].ToString().Length;
+                    }
+                }
+                return max;
+            }
+
+            /// <summary>
+            /// 整個class的核心，用來創建巴斯卡三角形矩陣
+            /// </summary>
+            /// <param name="n">層級</param>
+            /// <returns>巴斯卡三角形矩陣</returns>
+            private int[,] CreatePascalTri(int n)
+            {
+                int[,] Tri = new int[n + 1, n + 1];
+                for (int i = 0; i < Tri.GetLength(0); i++)
+                { Tri[0, i] = 1; Tri[i, 0] = 1; }
+                for (int i = 1; i < Tri.GetLength(0); i++)
+                {
+                    for (int j = 1; j < Tri.GetLength(1); j++)
+                    {
+                        Tri[i, j] = Tri[i - 1, j] + Tri[i, j - 1];
+                    }
+                }
+                return Tri;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = string.Empty;
+            int n = 0;
+            int.TryParse(textBox2.Text, out n);
+            PascalTriangle pascal = new PascalTriangle();
+            pascal.CreatePascal(n, textBox1);
+        }
+    }
+}
